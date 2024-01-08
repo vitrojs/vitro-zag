8f769a6b631974aa6a6dba7027791da83ac60b2e
dong.huo <huodon@gmail.com>
2024-01-08 13:33:55 +0800

    WIP

--- build.ts
+++ build.ts
@@ -1,15 +1,16 @@
 /// <reference types="bun-types" />
 
 import { buildSync } from 'esbuild'
-import pkg from './package.json' assert { type: 'json' }
-import * as path from 'node:path'
 import * as fs from 'node:fs'
-import * as url from 'node:url'
+import * as path from 'node:path'
+import pkg from './package.json' assert { type: 'json' }
 
-const dirname = path.dirname(url.fileURLToPath(import.meta.url))
-const dist = path.join(dirname, 'dist')
+const isProduction = Bun.env.NODE_ENV === 'production'
+const dist = path.join(import.meta.dir, 'dist')
+
 if (fs.existsSync(dist)) {
-  if (Bun.env.NODE_ENV === 'production') process.exit(0)
+  if (isProduction) process.exit(0)
+
   console.log(`🧹 Cleaning dist folder...`)
   fs.rmSync(dist, { recursive: true, force: true })
 }
